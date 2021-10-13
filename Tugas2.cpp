b564cd9d399d6152ebbfd4141476dbd143b1cf1c
riomulya <riomulya75@gmail.com>
2021-10-13 19:04:19 +0700

    menambahkan keterangan baru

--- Tugas2.cpp
+++ Tugas2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-  cout << "Rio Mulya Syawal" << endl;
+  cout << "Rio \"Mulya\" \t Syawal\n" << endl; //ini nama saya
   cout << "Saya Sedang Belajar C++" << endl;
   return 0;
 }
