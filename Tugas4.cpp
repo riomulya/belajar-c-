44ee052c0cf396cbc387a2ac354d00054f453fd5
riomulya <riomulya75@gmail.com>
2021-10-13 19:26:25 +0700

    increment dan decrement

---
+++ Tugas4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "increment dan decrement\n" << endl;
+  int X = 30;
+  cout << "Nilai Awal X : " << X << endl;
+  --X;
+  cout << "Nilai Kedua X : " << X << endl;
+  X++;
+  X++;
+  cout << "Nilai Ketiga X : " << X << endl;
+  return 0;
+}
