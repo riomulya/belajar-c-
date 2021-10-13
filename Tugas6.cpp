56a36a4d162afee5920306a762bb3a27b1811bf8
riomulya <riomulya75@gmail.com>
2021-10-13 20:08:59 +0700

    if kondisi

---
+++ Tugas6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x,y;
+  x = 5;
+  y = 20;
+  if (x>=y) {
+    cout << "hasil : " << x+y;
+  }else{
+    cout << "hasil : " << x-y;
+  }
+  return 0;
+}
