f8edf306ff05a41ad86a8771561ace4ae316c872
riomulya <riomulya75@gmail.com>
2021-10-13 19:13:23 +0700

    membuat operasi perkalian

---
+++ Tugas3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int A,B,C;
+  A = 10;
+  B = 20;
+  C = A * B;
+  cout << "Nilai Variabel A : " << A << endl;
+  cout << "Nilai Variabel B : " << B << endl;
+  cout << "Hasil Perkalian Kedua Variabel Adalah : " << C << endl;
+  return 0;
+}
