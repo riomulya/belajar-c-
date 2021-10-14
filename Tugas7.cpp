524ddb07342c30b8a553fa0e900d6af5735e4c58
riomulya <riomulya75@gmail.com>
2021-10-14 20:49:26 +0700

    menambahkan variabel baru (z)

--- Tugas7.cpp
+++ Tugas7.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int x,y;
+  int x,y,z;
   cout << "masukan nilai x : ";
   cin >> x;
   cout << "masukan nilai y : ";
   cin >> y;
+  cout << "masukan nilai z : ";
+  cin >> z;
+
   if (x%2==0) {
-    cout << "x adalah genap\n";
+    cout << "x adalah genap\n\n";
   }else{
-    cout << "x adalah ganjil\n";
+    cout << "x adalah ganjil\n\n";
   }
   if (y%2==0) {
-    cout << "y adalah genap\n";
+    cout << "y adalah genap\n\n";
   }else{
-    cout << "y adalah ganjil\n";
+    cout << "y adalah ganjil\n\n";
+  }
+  if (z%2==0) {
+    cout << "z adalah genap\n\n";
+  }else{
+    cout << "z adalah ganjil\n\n";
   }
   return 0;
 }
