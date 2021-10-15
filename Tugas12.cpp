b921d2b9a4a9f57f08d9a1cd99516aeb577dc2b9
riomulya <riomulya75@gmail.com>
2021-10-15 17:45:50 +0700

    Array satu Dimensi Dan Perulangannya Serta Penjumlahan

--- Tugas12.cpp
+++ Tugas12.cpp
@@ -4,6 +4,7 @@ int main() {
   int i,j;
   int x[3] = {1,5,4};
   int y[3] = {3,2,5};
+  cout << endl;
   for (i=0;i<3;i++)
   {
       cout << "Array x " << i << ": " << x[i] <<endl;
@@ -19,4 +20,5 @@ int main() {
       j = x[i]+y[i];
       cout << "Array Penjumlahan " << i << ": " << j <<endl;
   }
+  cout << endl;
 }   
