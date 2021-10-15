960b8b2d13795ca5e856b7742b6cd1c1bc0873c8
riomulya <riomulya75@gmail.com>
2021-10-15 13:45:16 +0700

    membuat dua buah prosedur

---
+++ Tugas10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int welcome(){
+    cout << "Selamat Datang ";
+}
+int konjungsidi(){
+    cout << "Di ";
+}
+int fungsikali(){
+    cout << "Fungsi Perkalian ";
+}
+int main() {
+    welcome();
+    konjungsidi();
+    fungsikali();
+  }
