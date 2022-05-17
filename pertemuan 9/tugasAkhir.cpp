528d58a23979256e1433a701301b657a9a1b8b89
RioMulya <riomulya75@gmail.com>
2022-05-17 15:37:37 +0700

    update pertemuan 9

---
+++ pertemuan 9/tugasAkhir.cpp
@@ -0,0 +1,18 @@
+// Rio Mulya Syawal
+// 211011401091
+// 02TPLP015
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char nama[100], hobi[100];
+    cout << "Masukkan Nama Anda : ";
+    gets(nama);
+    cout << "\nMasukkan Hobi Anda : ";
+    gets(hobi);
+    cout << endl;
+    cout << "Nama Anda Adalah : " << nama << endl;
+    cout << "Hobi Anda Adalah : " << hobi;
+}
