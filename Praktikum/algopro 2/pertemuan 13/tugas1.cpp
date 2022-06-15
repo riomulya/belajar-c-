f94498ebe053b49844afd017d03f2aafac2d4058
RioMulya <riomulya75@gmail.com>
2022-06-15 15:10:45 +0700

    Update

--- Praktikum/algopro 2/pertemuan 13/tugas1.cpp
+++ Praktikum/algopro 2/pertemuan 13/tugas1.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 void insertionSort(int data[], int length);
-void cetakArray(int data[], int length, string pesan);
+void cetakArray(int data[], int length, string pesanAwal, string pesanAkhir);
 
 int main()
 {
     SetConsoleTextAttribute(hstdout, 10);
     int i, data[] = {27, 25, 12, 32, 60, 52, 35, 37, 47, 17, 45, 10, 5, 15};
     const int dataSize = (sizeof(data) / sizeof(data[0])) - 1;
-    cetakArray(data, dataSize, "Sebelum Di Urutkan  : ");
+    cetakArray(data, dataSize, "Sebelum Di Urutkan  : ", "");
     insertionSort(data, dataSize);
     SetConsoleTextAttribute(hstdout, 10);
-    cetakArray(data, dataSize, "\n\nSesudah Di Urutkan  : ");
+    cetakArray(data, dataSize, "\n\nSesudah Di Urutkan  : ", "");
     getchar();
 }
 
@@ -34,12 +34,12 @@ void insertionSort(int data[], int length)
         data[j + 1] = temp;
 
         SetConsoleTextAttribute(hstdout, 12);
-        cout << "\n\nProses Sortingnya Ke - " << i << endl;
-        cetakArray(data, length, "-------------------------");
+        cout << "\nStep ke - " << i;
+        cetakArray(data, length, "\nProses Sorting-nya\n", "_______________________________________________________________________");
     }
 }
 
-void cetakArray(int data[], int length, string pesan)
+void cetakArray(int data[], int length, string pesan, string pesanAkhir)
 {
     cout << pesan << endl;
     for (int i = 0; i <= length; i++)
@@ -47,4 +47,5 @@ void cetakArray(int data[], int length, string pesan)
         cout << setw(5) << data[i];
     }
     cout << endl;
+    cout << pesanAkhir << endl;
 }
